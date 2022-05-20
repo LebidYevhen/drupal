31c1a3af2e838244029c55286dec9f10ea9d98b4
Yevhen Lebid <y.lebid@ydt-global.com>
2022-05-20 12:13:32 +0300

    task_30__main

--- web/modules/custom/yl_check_credentials/src/Controller/CheckCredentialsController.php
+++ web/modules/custom/yl_check_credentials/src/Controller/CheckCredentialsController.php
@@ -2,37 +2,23 @@
 
 namespace Drupal\yl_check_credentials\Controller;
 
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Drupal\Core\Controller\ControllerBase;
-
 /**
  * Provides route responses for the yl_check_credentials module.
  */
-class CheckCredentialsController extends ControllerBase {
+class CheckCredentialsController {
 
   /**
    * Returns a page.
    *
    * @return array
-   *   A simple renderable array or throw 403 error.
+   *   with theme hook data and access information.
    */
-  public function checkCredentialsAction(): array {
-    $current_user = \Drupal::currentUser()->getRoles();
-    $current_time = \Drupal::time()->getCurrentTime();
-    $minutes      = date('i', $current_time);
+  public function content(): array {
 
-    if ((in_array('manager', $current_user) && ($minutes % 2 == 0))
-          || (in_array('authenticated', $current_user) && ($minutes % 2 != 0))
-      ) {
-      return [
-        '#theme'       => 'yl_check_credentials',
-        '#access_info' => 'Access granted!',
-      ];
-    }
-    else {
-      throw new AccessDeniedHttpException();
-    }
+    return [
+      '#theme'       => 'yl_check_credentials',
+      '#access_info' => 'Access granted!',
+    ];
+  }
 
-  }//end checkCredentialsAction()
-
-}//end class
+}
