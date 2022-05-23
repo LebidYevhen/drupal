12680d3959e8f7971443aaf0c77349affb010cde
Yevhen Lebid <y.lebid@ydt-global.com>
2022-05-23 18:11:43 +0300

    task_93__main

--- web/modules/custom/yl_configuration_form/src/Form/ModuleConfigurationForm.php
+++ web/modules/custom/yl_configuration_form/src/Form/ModuleConfigurationForm.php
@@ -30,12 +30,15 @@ class ModuleConfigurationForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config               = $this->config('yl_configuration_form.admin_settings');
+    $config = $this->config('yl_configuration_form.admin_settings');
+
     $form['country_name'] = [
       '#type'          => 'select',
-      '#options'       => $this->getListOfCountries(),
+      '#options'       => $this->getTaxonomyTerms('country'),
       '#title'         => $this->t('Select a country'),
-      '#default_value' => $config->get('country_var'),
+      '#term_id'       => $config->get('term_id'),
+      '#terms'         => $config->get('terms'),
+      '#default_value' => $config->get('term_id'),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -46,29 +49,35 @@ class ModuleConfigurationForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('yl_configuration_form.admin_settings')
-         ->set('country_var', $form_state->getValue('country_name'))
-         ->save();
+      ->set('terms', $this->getTaxonomyTerms('country'))
+      ->set('term_id', $form_state->getValue('country_name'))
+      ->save();
     parent::submitForm($form, $form_state);
   }
 
   /**
-   * Get an array of country code => country name pairs.
+   * Gets an associative array of pairs term_id => term_name.
+   *
+   * @param string $term_name
+   *   Term name.
    *
    * @return array
-   *   An array of country code => country name pairs.
-   *
-   * @see \Drupal\Core\Locale\CountryManager::getStandardList()
+   *   An associative array of pairs term_id => term_name.
    */
-  public function getListOfCountries() {
-    $country_manager = \Drupal::service('country_manager');
-    $list            = $country_manager->getList();
-    $countries       = [];
-    foreach ($list as $key => $value) {
-      $val             = $value->__toString();
-      $countries[$key] = $val;
+  public function getTaxonomyTerms(string $term_name) {
+    $terms = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term')
+      ->loadTree($term_name);
+
+    $terms_data = [];
+    foreach ($terms as $term) {
+      $term_id   = $term->tid;
+      $term_name = $term->name;
+
+      $terms_data[$term_id] = $term_name;
     }
 
-    return $countries;
+    return $terms_data;
   }
 
 }
