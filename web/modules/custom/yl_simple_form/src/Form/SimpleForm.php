801d506e0b01684247d8d9d4f18c18c2eba56c05
Yevhen Lebid <y.lebid@ydt-global.com>
2022-11-15 15:43:59 +0200

    formatted according to the drupal standarts

--- web/modules/custom/yl_simple_form/src/Form/SimpleForm.php
+++ web/modules/custom/yl_simple_form/src/Form/SimpleForm.php
@@ -5,65 +5,66 @@ namespace Drupal\yl_simple_form\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
-class SimpleForm extends FormBase
-{
+/**
+ *
+ */
+class SimpleForm extends FormBase {
 
   /**
    * {@inheritdoc}
    */
-    public function getFormId()
-    {
-        return 'yl_simple_form';
-    }
+  public function getFormId() {
+    return 'yl_simple_form';
+  }
 
   /**
    * {@inheritdoc}
    */
-    public function buildForm(array $form, FormStateInterface $form_state)
-    {
-        $form['user_name'] = [
-        '#type' => 'textfield',
-        '#title' => t('User Name:'),
-        '#required' => true,
-        ];
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['user_name'] = [
+      '#type' => 'textfield',
+      '#title' => t('User Name:'),
+      '#required' => TRUE,
+    ];
 
-        $form['email'] = [
-        '#type' => 'textfield',
-        '#title' => t('Email:'),
-        '#required' => true,
-        ];
+    $form['email'] = [
+      '#type' => 'textfield',
+      '#title' => t('Email:'),
+      '#required' => TRUE,
+    ];
 
-        $form['submit'] = [
-        '#type' => 'submit',
-        '#value' => t('Submit'),
-        ];
-        return $form;
-    }
-
-  /**
-   * {@inheritdoc}
-   */
-    public function validateForm(array &$form, FormStateInterface $form_state)
-    {
-        $value = $form_state->getValue('email');
-
-        if ($value == !\Drupal::service('email.validator')->isValid($value)) {
-            $form_state->setErrorByName(
-                'email',
-                t('The email address %mail is not valid.', ['%mail' => $value])
-            );
-        }
-    }
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => t('Submit'),
+    ];
+    return $form;
+  }
 
   /**
    * {@inheritdoc}
    */
-    public function submitForm(array &$form, FormStateInterface $form_state)
-    {
-        $userName = $form_state->getValue('user_name');
-        $email = $form_state->getValue('email');
-        $message = "User Name: $userName, Email: $email";
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $value = $form_state->getValue('email');
 
-        \Drupal::logger('yl_simple_form')->info($message);
+    if ($value == !\Drupal::service('email.validator')->isValid($value)) {
+      $form_state->setErrorByName(
+        'email',
+        t('The email address %mail is not valid.', ['%mail' => $value])
+      );
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm( array &$form, FormStateInterface $form_state )
+
+  {
+    $userName = $form_state->getValue('user_name');
+    $email = $form_state->getValue('email');
+    $message = "User Name: $userName, Email: $email";
+
+    \Drupal::logger('yl_simple_form')->info($message);
+  }
+
 }
